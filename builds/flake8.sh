3d3057545fe10f5363f07662c4a7542fcbf641ef
mattjmorrison-imt <matt.morrison@imtapps.com>
2016-02-02 10:25:33 -0600

    Peripheral: fixed flake8 build

--- builds/flake8.sh
+++ builds/flake8.sh
@@ -6,4 +6,4 @@ source virtualenv/bin/activate
 
 pip install -r requirements.txt
 
-flake8 . --max-complexity=4 --exclude=virtualenv > flake8.txt
+flake8 . --max-complexity=4 --exclude=virtualenv | sed 's|^./||' > flake8.txt
